18ead6193cb5e3b5f30f6698923ce06fb02ffb22
Mohit Gadkari <mohit.gadkari@teradata.com>
2019-03-09 14:01:26 -0800

    ML changes

--- src/app/ml/win_predictor.py
+++ src/app/ml/win_predictor.py
@@ -17,8 +17,8 @@ from sklearn.svm import SVC
 
 
 # Load dataset
-url = "./mlb.csv"
-names = ['Win','Loss', 'Win Ratio','Team']
+url = "./mlb6.csv"
+names = ['Win','Loss', 'Win Ratio','Team' ]
 dataset = pandas.read_csv(url, names=names)
 
 # shape
@@ -42,47 +42,56 @@ array = dataset.values
 X = array[:,0:3]
 Y = array[:,3]
 validation_size = 0.30
-seed = 4
+seed = 12
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 #Y_train=Y_train.astype('int')
 
 
 # Test options and evaluation metric
-seed = 4
+seed = 5
 scoring = 'accuracy'
 
 # Spot Check Algorithms
 models = []
 models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
-#models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
 models.append(('KNN', KNeighborsClassifier()))
-# models.append(('CART', DecisionTreeClassifier()))
-# models.append(('NB', GaussianNB()))
-# models.append(('SVM', SVC(gamma='auto')))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
 # evaluate each model in turn
 results = []
 names = []
 for name, model in models:
-	kfold = model_selection.KFold(n_splits=4, random_state=seed)
+	kfold = model_selection.KFold(n_splits=20, random_state=seed)
 	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
 	results.append(cv_results)
 	names.append(name)
 	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
 	print(msg)
 
-# Compare Algorithms
-# fig = plt.figure()
-# fig.suptitle('Algorithm Comparison')
-# ax = fig.add_subplot(111)
-# plt.boxplot(results)
-# ax.set_xticklabels(names)
-# plt.show()
+#Compare Algorithms
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
 
 # Make predictions on validation dataset
-knn = KNeighborsClassifier()
-knn.fit(X_train, Y_train)
-predictions = knn.predict(X_validation)
-print(accuracy_score(Y_validation, predictions))
-print(confusion_matrix(Y_validation, predictions))
-print(classification_report(Y_validation, predictions))
-print(len(predictions))
+# knn = KNeighborsClassifier()
+# knn.fit(X_train, Y_train)
+# predictions = knn.predict(X_validation)
+# print(accuracy_score(Y_validation, predictions))
+# #print(confusion_matrix(Y_validation, predictions))
+# print(classification_report(Y_validation, predictions))
+# print(len(predictions))
+
+dn= DecisionTreeClassifier()
+dn.fit(X_train, Y_train)
+predictions_d=dn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions_d))
+print(confusion_matrix(Y_validation, predictions_d))
+print(classification_report(Y_validation, predictions_d))
+print(len(predictions_d))
+
